d1613edb2c27c0f4a9b704421b91fe2b616a2958
lunaInsidious <itsukitakatsuno@gmail.com>
2026-01-22 07:31:47 +0900

    ok

--- src/ws/run.sh
+++ src/ws/run.sh
@@ -6,8 +6,14 @@ host="${1:-localhost}"
 ips=($(dig "$host" A +short))
 
 for ip in "${ips[@]}"; do
-    # もしhost名/ip名のディレクトリがなければ作成
-    mkdir -p "heapsnapshots/$host/$ip"
-    output_path="heapsnapshots/$host/$ip/$(date +%Y%m%d_%H%M%S).heapsnapshot"
+    # これからoutputする予定のディレクトリがあるか確認
+    # heapsnapshots/host/(date +%Y%m%d_%H%M%S)-ipの形で、dateの部分は作成時刻なので、毎回変わる。正規表現でマッチさせる
+    output_dir=$(find heapsnapshots/"$host"/ -type d -regextype posix-extended -regex ".*/[0-9]{8}_[0-9]{6}-$ip" | head -n 1 || true)
+    if [ -z "$output_dir" ]; then
+        # なければ新規作成
+        output_dir=heapsnapshots/"$host"/"$(date +%Y%m%d_%H%M%S)"-"$ip"
+        mkdir -p "$output_dir"
+    fi
+    output_path="$output_dir/$(date +%Y%m%d_%H%M%S).heapsnapshot"
     node ./take-heapsnapshot.mjs "$ip":9229 "$output_path"
 done
